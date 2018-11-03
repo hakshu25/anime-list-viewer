d5cce23ae7dde5bf565c26e8eed6eaede797488b
flyhighair <flyhighup.air25@gmail.com>
2018-11-03 22:37:26 +0900

    add anime title list

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AnimeList header="this is anime lists" :lists="lists"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AnimeList from "./components/AnimeList.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    AnimeList
+  },
+  data: () => {
+    return {
+      lists: []
+    };
+  },
+  mounted () {
+    axios
+    .get("http://api.moemoe.tokyo/anime/v1/master/2018")
+    .then(res => (this.lists = res.data))
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/AnimeList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="anime">
+        <h1>{{ header }}</h1>
+        <ul>
+            <li v-for="listItem in lists" :key="listItem.id">
+                {{ listItem.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AnimeList',
+    props: {
+        header: String,
+        lists: Array,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
