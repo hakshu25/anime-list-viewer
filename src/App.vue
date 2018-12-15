82e88494f0b074f2b692b04622321b1bd3979d40
flyhighair <flyhighup.air25@gmail.com>
2018-12-15 18:08:23 +0900

    add selecting date implements

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <AnimeList header="this is anime lists" :lists="lists"/>
+    <AnimeList/>
   </div>
 </template>
 
 <script>
 import AnimeList from "./components/AnimeList.vue";
-import axios from "axios";
 
 export default {
   name: "app",
   components: {
     AnimeList
   },
-  data: () => {
-    return {
-      lists: []
-    };
-  },
-  mounted () {
-    axios
-    .get("http://api.moemoe.tokyo/anime/v1/master/2018")
-    .then(res => (this.lists = res.data))
-  }
 };
 </script>
 
