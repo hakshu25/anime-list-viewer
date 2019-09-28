35ba8e61c68f938e1a60d13def53741d53524050
hakshu <32229790+flyhighair@users.noreply.github.com>
2019-09-28 16:17:50 +0900

    Feature/add anime list (#1)
    * add anime title list
    
    * add selecting date implements
    
    * yarn upgrade
    
    * Remove helloworld component

---
+++ src/components/AnimeList.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="anime">
+    <select id="year">
+      <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
+    </select>
+    <select id="cour">
+      <option value="1">春アニメ</option>
+      <option value="2">夏アニメ</option>
+      <option value="3">秋アニメ</option>
+      <option value="4">冬アニメ</option>
+    </select>
+    <button type="button" v-on:click="displayAnimeList">表示</button>
+    <v-client-table :data="rows" :columns="columns"></v-client-table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "AnimeList",
+  data: () => {
+    return {
+      columns: ["title"],
+      rows: [],
+      years: []
+    };
+  },
+  methods: {
+    getCourInfo: month => {
+      const cours = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
+      let index = cours.findIndex(v => {
+        return v[0] <= month && month <= v[2];
+      });
+      return ++index;
+    },
+    createYears: () => {
+      const minDate = new Date('2014');
+      const firstYear = minDate.getFullYear();
+      const maxDate = new Date();
+      const through = maxDate.getFullYear() + 1 - firstYear;
+      const years = [];
+      for (let i = firstYear, length = firstYear + through; i < length; i++) {
+        years.push(i);
+      }
+      return years;
+    },
+    fetchAnimeList: function(year, cour) {
+      axios
+        .get(`http://api.moemoe.tokyo/anime/v1/master/${year}/${cour}`)
+        .then(res => {
+          this.rows = res.data;
+        })
+        .catch(e => console.log(e));
+    },
+    displayAnimeList: function () {
+        const year = document.getElementById('year').value;
+        const cour = document.getElementById('cour').value;
+        this.fetchAnimeList(year, cour);
+    }
+  },
+  mounted() {
+    this.years = this.createYears();
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const cour = this.getCourInfo(month);
+    this.fetchAnimeList(year, cour);
+  }
+};
+</script>
+
+<style scoped>
+</style>
