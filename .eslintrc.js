9bfa44a1aabcce79bdb2f50b99c55ea45e38647b
flyhighair <flyhighup.air25@gmail.com>
2019-10-11 20:18:09 +0900

    Add @vue/cli-plugin-typescript from vue-cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,8 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: ["plugin:vue/recommended", "eslint:recommended"],
-  rules: {},
+  'extends': [
+    '@vue/typescript'
+  ],
   parserOptions: {
-    parser: "babel-eslint"
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    parser: '@typescript-eslint/parser'
+  }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <AnimeList />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
-<script>
-import AnimeList from "./components/AnimeList.vue";
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: "App",
+@Component({
   components: {
-    AnimeList
+    HelloWorld,
   },
-};
+})
+export default class App extends Vue {}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
