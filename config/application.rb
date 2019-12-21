1584fc6fc8c2dd3ec715214a37ffb606f4c3f7ad
hakshu <flyhighup.air25@gmail.com>
2019-12-22 08:57:50 +0900

    Feature/add anime list api (#17)
    * Add RSpec
    
    * Remove ActiveRecord code
    
    * Add animelist api definition
    
    * add anime_list model
    
    * add anime list api
    
    * Refactor anime list model
    
    * Add parameter constraint to API
    
    * Fix end
    
    * Add AnimeListHelper
    
    * Remove puts

--- config/application.rb
+++ config/application.rb
@@ -5,15 +5,15 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 # require "active_record/railtie"
-require "active_storage/engine"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -33,5 +33,7 @@ module AnimeListViewer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_controller.include_all_helpers = false
   end
 end
