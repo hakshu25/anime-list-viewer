d89d0134ac7354bcbfefd1c458027e06b25d029a
flyhighair <flyhighup.air25@gmail.com>
2019-12-22 08:26:58 +0900

    Add parameter constraint to API

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   defaults format: :json do
     namespace :api do
-      get 'anime_lists/:year/:cour' => 'anime_lists#show'
+      get 'anime_lists/:year/:cour' => 'anime_lists#show',
+          constraints: {
+            year: /\d{4}/,
+            cour: /\d{1}/
+          }
     end
   end
 end
--- spec/requests/api/anime_lists_spec.rb
+++ spec/requests/api/anime_lists_spec.rb
@@ -4,26 +4,27 @@ require 'rails_helper'
 
 RSpec.describe 'AnimeLists', type: :request do
   describe 'GET api/anime_lists' do
-    before do
-      data = [{
-        'TID' => '1',
-        'Title' => 'anime',
-        'Links' => [['http://hoge.com', 'オフィシャルサイト']]
-      }]
-      allow(AnimeList).to receive(:term).and_return(data)
-      get '/api/anime_lists/2019/1'
-    end
+    context 'request successful' do
+      before do
+        data = [{
+          'TID' => '1',
+          'Title' => 'anime',
+          'Links' => [['http://hoge.com', 'オフィシャルサイト']]
+        }]
+        allow(AnimeList).to receive(:term).and_return(data)
+        get '/api/anime_lists/2019/1'
+      end
 
-    it 'status 200' do
-      expect(response).to have_http_status(200)
-    end
+      it 'status 200' do
+        expect(response).to have_http_status(200)
+      end
 
-    it 'valid response' do
-      expect(JSON.parse(response.body).map(&:deep_symbolize_keys)).to eq [{
-        id: 1,
-        title: 'anime',
-        link: 'http://hoge.com'
-      }]
+      it 'valid response' do
+        expect(JSON.parse(response.body).map(&:deep_symbolize_keys)).to eq [{
+          id: 1,
+          title: 'anime',
+          link: 'http://hoge.com'
+        }]
+      end
     end
-  end
 end
